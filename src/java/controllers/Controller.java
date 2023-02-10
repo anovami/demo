9a0e1971827f0dfdece7c0f91e616f6929b176ac
Maria Antonova <antonovami.work@gmail.com>
2023-02-10 16:10:38 +0300

    2

--- src/java/controllers/Controller.java
+++ src/java/controllers/Controller.java
@@ -10,14 +10,7 @@ public class Controller {
     public static void main(String[] args) {
         Service service = new Service();
         System.out.println(service.getResultModel());
-//        String text = "2018-05-24_12:04:13.889";
-//
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss.SSS");
-//
-//        LocalDateTime time;
-//        time = LocalDateTime.parse(text, formatter);
-//        System.out.println(text);
-//        this.timeStart = LocalDateTime.parse(timeStart, formatter);
+
 
     }
 }
